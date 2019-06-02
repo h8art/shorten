bbad4ed460450c6f5c8a41000135f55d8cc2836b
h8art <artem.gershteyn@gmail.com>
2019-06-02 13:48:40 +0300

    add constructor

--- urlShorten.go
+++ urlShorten.go
@@ -5,32 +5,35 @@ import (
 	"fmt"
 )
 
-type MyShortener struct {
+type MyShortened struct {
 	storage map[string]string
 }
 
-type Shortener interface {
+type Shortened interface {
 	Shorten(string) string
 	Resolve(string) string
 }
 
-func (s MyShortener) Shorten(url string) string {
+func (s MyShortened) Shorten(url string) string {
 	src := []byte(url)
 	s.storage[hex.EncodeToString(src)] = url
 	return hex.EncodeToString(src)
 }
 
-func (s MyShortener) Resolve(url string) string {
+func (s MyShortened) Resolve(url string) string {
 	return s.storage[url]
 }
+func NewMyShortener() *MyShortened {
+	shortener := new(MyShortened)
+	shortener.storage = make(map[string]string)
+	return shortener
+}
+func Convert(u string) {
+	tempUrls := NewMyShortener()
+	urlShorten := tempUrls.Shorten(u)
 
-func convert(u string) {
-	tempUrls := new(MyShortener)
-	tempUrls.storage = make(map[string]string)
-
-	urlShorten := Shortener.Shorten(tempUrls, u)
 	fmt.Println(urlShorten)
 
-	urlResoled := Shortener.Resolve(tempUrls, urlShorten)
+	urlResoled := tempUrls.Resolve(urlShorten)
 	fmt.Println(urlResoled)
 }
